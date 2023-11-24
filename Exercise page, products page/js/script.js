b41c6217db6f891d65c626a4ad9f5747b9e80168
samira dorchalian <samiradorchalain1998@gmail.com>
2023-11-24 16:12:52 +0330

    Exercise page, products page

---
+++ Exercise page, products page/js/script.js
@@ -0,0 +1,77 @@
+const searchInput = document.getElementById("search-input");
+const products = document.querySelectorAll(".product-item");
+const buttons = document.querySelectorAll(".filter");
+// const priceDiv = document.getElementById("search-price");
+// const priceButton = priceDiv.querySelector("button");
+const priceButton = document
+  .getElementById("search-price")
+  .querySelector("button");
+
+const changeClass = (filter) => {
+  buttons.forEach((button) => {
+    if (button.dataset.filter === filter) {
+      button.classList.add("selected");
+    } else {
+      button.classList.remove("selected");
+    }
+  });
+};
+
+const searchHandler = (event) => {
+  const searchValue = event.target.value.toLowerCase().trim();
+
+  products.forEach((product) => {
+    const productName = product.children[1].innerText.toLowerCase();
+    if (productName.includes(searchValue)) {
+      product.style.display = "block";
+    } else {
+      product.style.display = "none";
+    }
+  });
+};
+
+const filterHandler = (event) => {
+  const filter = event.target.dataset.filter;
+  changeClass(filter);
+
+  products.forEach((product) => {
+    const category = product.dataset.category;
+    if (filter === "all") {
+      product.style.display = "block";
+    } else {
+      filter === category
+        ? (product.style.display = "block")
+        : (product.style.display = "none");
+      //   if (filter === category) {
+      //     product.style.display = "block";
+      //   } else {
+      //     product.style.display = "none";
+      //   }
+    }
+  });
+};
+
+const searchPeiceHandler = (event) => {
+  const searchPrice = +event.target.parentElement.children[0].value;
+  products.forEach((product) => {
+    const productPrice = product.children[2].innerText;
+    const price = +productPrice.split(" ")[1];
+
+    if (!searchPrice) {
+      product.style.display = "block";
+    } else {
+      searchPrice === price
+        ? (product.style.display = "block")
+        : (product.style.display = "none");
+    }
+  });
+};
+const start = () => {
+  buttons.forEach((button) => {
+    button.addEventListener("click", filterHandler);
+  });
+  searchInput.addEventListener("keyup", searchHandler);
+  priceButton.addEventListener("click", searchPeiceHandler);
+};
+
+window.addEventListener("load", start);
