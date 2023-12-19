12f0e9ec7c50f4a3e88940ab0b1df122f2b5bd44
samira dorchalian <samiradorchalain1998@gmail.com>
2023-12-19 13:29:44 +0330

    card-7

---
+++ Card-7/script.js
@@ -0,0 +1,7 @@
+const cardfanEl = document.querySelector(".cardfan");
+
+cardfanEl.innerHTML = `
+    <img src="./image/24.jpg" alt="Photograph1" id="Castle">
+    <img src="./image/26.jpg" alt="Photograph2" id="balloon">
+    <img src="./image/34.jpg" alt="Photograph3" id="City">
+`;
